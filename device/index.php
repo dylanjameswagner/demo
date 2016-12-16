d2627a6bfa96c711370a6dcad567ebc1b0965560
Dylan James Wagner <dylan@teamcolab.com>
2016-12-16 12:07:37 -0500

    add device

---
+++ device/index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<meta name="viewport" content="width=device-width,minimum-scale=0.5,maximum-scale=1.0,user-scalable=no"/>
+
+	<meta name="apple-mobile-web-app-capable" content="default"/>
+
+	<title>device</title>
+
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./-/images/apple-touch-icon-144x144-precomposed.png"/>
+
+	<link rel="stylesheet" href="./-/styles/core.css"/>
+	<link rel="stylesheet" href="./-/styles/custom.css"/>
+
+	<script src="./-/scripts/html5shiv.printshiv-3.6.2.min.js"></script>
+	<script src="./-/scripts/prefixfree-1.0.7.min.js"></script>
+
+<!--[if IE 8]><script src="./-/scripts/selectivizr-1.0.2.min.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+	<noscript>
+		<p>Enable JavaScript to run tests.</p>
+	</noscript>
+
+	<div id="top">
+
+		<div id="content">
+
+			<table class="measurements">
+				<tr><th>useragent</th>		<td class="useragent"><script>document.write(navigator.userAgent);</script></td></tr>
+				<tr><th>ratio</th>			<td class="ratio">@<script>document.write(('devicePixelRatio' in window) ? devicePixelRatio : 1);</script>x</td></tr>
+				<tr><th>orientation</th>	<td class="orientation"></td>
+				<tr><th>pixels</th>
+				<td>
+					<script>
+						var ratio = ('devicePixelRatio' in window) ? devicePixelRatio : 1;
+						document.write(Math.ceil(ratio * screen.width) + ' × ' + Math.ceil(ratio * screen.height));
+					</script>
+				</td></tr>
+				<tr><th>screen</th>			<td class="screen"></td></tr>
+				<tr><th>viewport</th>		<td class="viewport"></td></tr>
+				<tr><th>document</th>		<td class="document"></td></tr>
+			</table>
+
+			<table class="dimensions">
+				<caption>CSS Measurments</caption>
+
+				<thead>
+					<tr><th>Device</th>			<th>Screen</th>		<th>Viewport</th>	<th>Browser</th>		<th>Orientation</th></tr>
+				</thead>
+
+				<tbody>
+
+					<tr><th>iPad</th>			<td>768 × 1024</td>	<td>768 × 928</td>	<td><small>iOS 7 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPad</th>			<td>1024 × 768</td>	<td>1024 × 672</td>	<td><small>iOS 7 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPad Mini</th>		<td>768 × 1024</td>	<td>768 × 928</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPad Mini</th>		<td>1024 × 768</td>	<td>1024 × 671</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPad Pro</th>		<td>1024 × 1366</td> <td>1024 × ???</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPad Pro</th>		<td>1366 × 1024</td> <td>??? × 1024</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPhone 6 Plus</th>	<td>360 × 640</td>	<td>360 × ?</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPhone 6 Plus</th>	<td>640 × 360</td>	<td>? × ?</td>		<td><small>iOS 8 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPhone 6</th>		<td>375 × 667</td>	<td>375 × 559</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPhone 6</th>		<td>667 × 375</td>	<td>667 × 331</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPhone 5</th>		<td>320 × 568</td>	<td>320 × 460</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPhone 5</th>		<td>568 × 320</td>	<td>568 × 212</td>	<td><small>iOS 8 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+
+					<tr><th>iPhone</th>			<td>320 × 480</td>	<td>320 × 372</td>	<td><small>iOS 7 Safari</small></td>	<td class="glyph portrait"  title="portrait"></td></tr>
+					<tr><th>iPhone</th>			<td>480 × 320</td>	<td>480 × 212</td>	<td><small>iOS 7 Safari</small></td>	<td class="glyph landscape"	title="landscape"></td></tr>
+<?php /*
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+					<tr><td colspan="5">&nbsp;</td></tr>
+*/ ?>
+				</tbody>
+
+			</table>
+
+		</div>
+
+	</div><!--#top-->
+
+	<script src="./-/scripts/jquery-1.11.0.min.js"></script>
+	<script src="./-/scripts/custom.js"></script>
+
+</body>
+</html>
